3f3e1bb7c5865fdc97bd4b26462810dda3ed3429
ketai4eg <starikevichm.other@gmail.com>
2023-01-22 19:58:43 +0000

    docker and tests v2

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 #
 # load_dotenv()
 
-PG_USER = os.getenv('PG_USER', 'admin')
-PG_PASSWORD = os.getenv('PG_PASSWORD', 'admin1')
-PG_HOST = os.getenv('PG_HOST', 'test_db')
-PG_PORT = os.getenv('PG_PORT', 5432)
-PG_DB = os.getenv('PG_DB', 'test_db')
+PG_USER = os.getenv('PG_USER', 'admin1')
+PG_PASSWORD = os.getenv('PG_PASSWORD', 'admin111')
+PG_HOST = os.getenv('PG_HOST', '127.0.0.1')
+PG_PORT = os.getenv('PG_PORT', 5433)
+PG_DB = os.getenv('PG_DB', 'app_db')
 
 PG_DSN = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}'
